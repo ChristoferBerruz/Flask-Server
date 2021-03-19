71980065615a9afccbf3711cae25142ad2c6105b
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-18 22:09:26 -0400

    Connect database to Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from .database import init_database
 
 app = Flask(__name__)
+init_database()
 
 if __name__ == "main":
     app.run()
