84690df7a4f4f98a345edc46e0897507cda0cd09
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-22 12:27:48 -0400

    JWT Secret from environment

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,11 +1,21 @@
 # Configuration objects depending on environment to run
 
 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+JWT_SKEY = os.environ.get("SECRET_KEY")
+
 '''
 Base configuration to be extended by all other configurations
 '''
 class BaseConfig():
     API_PREFIX = '/api/v1'
+    SECRET_KEY = JWT_SKEY
     TESTING = False
     DEBUG = False
 
