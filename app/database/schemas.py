3dc784e5fae5519c928904bdcbfc5fc8329075f9
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-22 10:57:28 -0400

    Marshmallow schemas

---
+++ app/database/schemas.py
@@ -0,0 +1,26 @@
+# Schemas representation of the database models
+# to be used across the program
+
+from marshmallow import Schema, fields, ValidationError
+
+class AdminSchema(Schema):
+    id = fields.Integer()
+    firstname = fields.Str()
+    lastname = fields.Str()
+    organization = fields.Str()
+    email = fields.Email()
+    devices = fields.List(fields.Integer())
+    password = fields.Str()
+
+class RecordingDeviceSchema(Schema):
+    id = fields.Integer()
+    name = fields.Str()
+    location = fields.Str()
+    date_installed = fields.DateTime()
+    admin = fields.Integer()
+
+class HanwashingRecordSchema(Schema):
+    id = fields.Integer()
+    timestamp = fields.DateTime()
+    duration = fields.Float()
+    device = fields.Integer()
