332c3c549e4c3243cba87df51d906afaac45906b
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-22 12:28:26 -0400

    JWT added and some routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from app.database import init_database
 from flask_restful import Api
-from app.api.resources.handwashing import HandwashingRecordItem
+from app.api.resources.handwashing import HandwashingRecordItem, HandwashingRecords
+from app.api.resources.admin import AdminInfo
+from app.api.resources.auth import Login
+from flask_jwt_extended import JWTManager
+
+from app.config import config
 
 # Create flask app
 app = Flask(__name__)
 
+app.config.from_object(config)
+
+# Add auth service
+jwt = JWTManager(app)
+
 # Make app a REST api
-api = Api(app, prefix='/v1')
+api = Api(app, prefix=config.API_PREFIX)
 
 # Connnect to database
 init_database()
@@ -15,6 +25,9 @@ init_database()
 # Add enpoints
 api.add_resource(HandwashingRecordItem, '/handwashing-record/<int:record_id>', '/handwashing-record')
 
+api.add_resource(HandwashingRecords, '/handwashing-record/all/<int:device_id>')
+api.add_resource(AdminInfo, '/admin/<int:admin_id>', '/admin')
+api.add_resource(Login, '/login')
 if __name__ == "main":
     app.run()
 
